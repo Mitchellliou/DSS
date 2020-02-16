f638b0215db497198f54f9b73502a8085ce10edc
Mitchell Liou <mliou@vt.edu>
2020-02-16 01:33:47 -0500

    Added liquid gauges

--- ReactJS/dss-dashboard/src/App.js
+++ ReactJS/dss-dashboard/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
 import Graph from './Graph'
-// import { db } from './firebase'
-// import firebase from './firebase'
-
-// db.collection("sampleData")
-// .get()
-// .then(querySnapshot => {
-//   const data = querySnapshot.docs.map(doc => doc.data());
-//   console.log(data); // array of cities objects
-// });
-
 
 function App() {
   return (
     <div className="App">
-      <Graph/>
+      <Graph param="Distance"/>
+      <Graph param="Teststring"/>
     </div>
   );
 }
--- ReactJS/dss-dashboard/src/Graph.js
+++ ReactJS/dss-dashboard/src/Graph.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import { color } from 'd3-color';
+import { interpolateRgb } from 'd3-interpolate';
+import React, { Component } from 'react';
 
+import LiquidFillGauge from 'react-liquid-gauge';
 import firebase from './firebase'
 
 class Graph extends Component {
+    startColor = '#ff0000'; // red 
+    endColor = '#7CFC00'; // lawn green
+
     constructor(props) {
         super(props);
+
         this.state = {
-            d: 0
+            value: 0
         }
     };
 
@@ -14,29 +21,100 @@ class Graph extends Component {
         const db = firebase.firestore();
 
         db.collection("sampleData").doc("sampleDoc").onSnapshot((doc) => {
-            var data = doc.data()['Distance'];
+            var data = doc.data()[this.props.param];
             console.log(data);
             this.setState({
-                d: data
+                value: data
             });
         });
     };
 
     render() {
-        if (this.state.d < 3) {
-            return (
-                <div>
-                    {this.state.d}
+        const radius = 200;
+        const interpolate = interpolateRgb(this.startColor, this.endColor);
+        const fillColor = interpolate(this.state.value / 100);
+        const gradientStops = [
+            {
+                key: '0%',
+                stopColor: color(fillColor).darker(0.5).toString(),
+                stopOpacity: 1,
+                offset: '0%'
+            },
+            {
+                key: '50%',
+                stopColor: fillColor,
+                stopOpacity: 0.75,
+                offset: '50%'
+            },
+            {
+                key: '100%',
+                stopColor: color(fillColor).brighter(0.5).toString(),
+                stopOpacity: 0.5,
+                offset: '100%'
+            }
+        ];
+
+        return (
+            <div>
+                <LiquidFillGauge
+                    style={{ margin: '0 auto' }}
+                    width={radius * 2}
+                    height={radius * 2}
+                    value={this.state.value}
+                    percent="%"
+                    textSize={1}
+                    textOffsetX={0}
+                    textOffsetY={0}
+                    textRenderer={(props) => {
+                        const value = Math.round(props.value);
+                        const radius = Math.min(props.height / 2, props.width / 2);
+                        const textPixels = (props.textSize * radius / 2);
+                        const valueStyle = {
+                            fontSize: textPixels
+                        };
+                        const percentStyle = {
+                            fontSize: textPixels * 0.6
+                        };
+
+                        return (
+                            <tspan>
+                                <tspan className="value" style={valueStyle}>{value}</tspan>
+                                <tspan style={percentStyle}>{props.percent}</tspan>
+                            </tspan>
+                        );
+                    }}
+                    riseAnimation
+                    waveAnimation
+                    waveFrequency={2}
+                    waveAmplitude={1}
+                    gradient
+                    gradientStops={gradientStops}
+                    circleStyle={{
+                        fill: fillColor
+                    }}
+                    waveStyle={{
+                        fill: fillColor
+                    }}
+                    textStyle={{
+                        fill: color('#444').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    waveTextStyle={{
+                        fill: color('#fff').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                />
+                <div
+                    style={{
+                        margin: '20px auto',
+                        width: 120
+                    }}
+                >
+
                 </div>
-            );
-        } else {
-            return (
-                <div style={{backgroundColor:'red'}}>
-                    {this.state.d}
-                </div>
-            );
-        }
-    };
+            </div>
+        );
+    }
 }
 
 
